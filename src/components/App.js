efb725677fd7f7829da6385600b941fab4d1b0ec
Daniel <danielezra10@gmail.com>
2017-09-26 19:05:16 +0300

    changed struct

--- src/components/App.js
+++ src/components/App.js
@@ -13,42 +13,6 @@ const uiTheme = getMuiTheme({
     isRtl: true
 });
 
-// let items = [
-//     {  
-//         "depth":0,
-//         "children":[],
-//         "_shouldRender":false
-//     },
-//     {
-//         "depth": 1,
-//         "children": [2,4],
-//         "title": "emglis",
-//         "parentIndex": 0
-//     },
-//     {
-//         "depth": 2,
-//         "children": [3],
-//         "title": "בצפון",
-//         "parentIndex": 1
-//     },
-//     {
-//         "depth": 3,
-//         "title": "דפוק",
-//         "parentIndex": 2
-//     },
-//     {
-//         "depth": 2,
-//         "title": "בעזעזל",
-//         "parentIndex": 1
-//     },
-//     {
-//         "depth": 1,
-//         "title": "נו כבר",
-//         "parentIndex": 0
-//     }
-// ];
-
-
 let data = [
     {
       title: "אלעד",
@@ -111,7 +75,6 @@ const Sidebar = () => {
     <div>
       <MuiThemeProvider muiTheme={uiTheme} >
           <div className="hebrew">
-              <Header title="הגינה של סלדג"/>
               <div className="side-menu">
                 <TreeView data={data} />
               </div>
@@ -125,6 +88,9 @@ class App extends React.Component {
     render() {
         return (
         <div>
+          <MuiThemeProvider muiTheme={uiTheme} >
+            <Header title="הגינה של סלדג" className="hebrew"/>
+          </MuiThemeProvider>
           <ProfHeader/>
           <Sidebar/>
         </div>
