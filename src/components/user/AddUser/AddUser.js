07b2e77a23764afa75f486699090ecee01f29253
weizmm <weizmm05@gmail.com>
2018-05-03 13:30:29 +0300

    with Tabs

--- src/components/user/AddUser/AddUser.js
+++ src/components/user/AddUser/AddUser.js
@@ -4,13 +4,13 @@ import Tabs from '../../shared/Tabs/Tabs';
 
 class AddUser extends Component{
 
-render(){
-    return(
-        <React.Fragment>
-           <Tabs/>
-        </React.Fragment>
-    )
-}
+    render(){
+        return(
+            <React.Fragment>
+                <Tabs/>
+            </React.Fragment>
+        )
+    }
 
 }
 
